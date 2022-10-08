8de8e534f9b18bdc8c5ae2bfde2e9ec445aa0cba
Amreen <wafaamreen6@gmail.com>
2022-10-08 20:04:00 +0530

    updated

--- Lec practice problem/program4.c
+++ Lec practice problem/program4.c
@@ -1,4 +1,4 @@
-//WAP to enter 3times in a loop if  the user enter even no  
+//WAP to enter 3times in a loop if  the user enter even no atleast one time then it should print you won 
 #include<stdio.h>
 int main()
 {
